4119af35809aba820ffa0dca23153a8628d79952
Arthur Bailão <arthurbailao@gmail.com>
2016-10-09 23:13:10 -0300

    add queues

---
+++ queues/Deque.java
@@ -0,0 +1,41 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+    public Deque() {
+
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void addFirst(Item item) {
+
+    }
+
+    public void addLast(Item item) {
+
+    }
+
+    public Item removeFirst() {
+        return null;
+    }
+
+    public Item removeLast() {
+        return null;
+    }
+
+    public Iterator<Item> iterator() {
+
+    }
+
+    private class
+
+    public static void main(String[] args) {
+
+    }
+}
